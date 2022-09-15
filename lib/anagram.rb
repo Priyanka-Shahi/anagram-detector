8b95b31878b74d4c10a9c64d7d6de5768daaa728
Priyanka-Shahi <shahipriyan@gmail.com>
2022-09-15 15:45:41 +1000

    commit tree 665d70c403c7d11c30680baa549c307098b8a569

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+
+    attr_reader :word
+  
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(array)
+      real_anagram = array.select {|option| option.split("").sort == @word.split("").sort && option.size == word.size }
+    end
+  
+  end
